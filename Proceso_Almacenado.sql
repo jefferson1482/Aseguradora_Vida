85379588d538705c18726ed8468ec1970700d65d
jefferson1482 <50092499+jefferson1482@users.noreply.github.com>
2021-03-10 17:20:17 -0500

    Add files via upload

---
+++ Proceso_Almacenado.sql
@@ -0,0 +1,16 @@
+﻿--Procedimiento almacenado que devuelva el total de todas las facturas realizadas por la empresa.
+CREATE OR REPLACE FUNCTION public.report_fac_total()
+  RETURNS money AS
+$BODY$DECLARE total money;
+
+BEGIN
+SELECT 
+  sum (cast(factura.fac_total as money)) INTO total 
+FROM 
+  public.factura;
+RETURN total;
+END;$BODY$
+  LANGUAGE plpgsql
+
+
+--select public.report_fac_total()
