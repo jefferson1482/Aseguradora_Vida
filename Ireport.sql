b7213f9f52fbae2df64fbc7a6881f25f0990598c
jefferson1482 <50092499+jefferson1482@users.noreply.github.com>
2021-03-10 20:14:40 -0500

    Add files via upload

---
+++ Ireport.sql
@@ -0,0 +1,9 @@
+﻿SELECT 
+  empresa.emp_nombre as Empresa, 
+  count(factura.fac_num) as Cantidad_de_Facturas,
+  factura.fac_fecha as Factura
+FROM 
+  public.empresa
+  inner join public.factura on empresa.emp_id = factura.emp_id
+  
+  group by (Empresa,Factura);
